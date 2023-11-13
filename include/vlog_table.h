570b49d2f9000547d75c8e632b48ecb0cc876f0b
superyyf <1420328500@qq.com>
2023-11-13 08:32:43 +0800

    create class

--- include/vlog_table.h
+++ include/vlog_table.h
@@ -2,33 +2,39 @@
 #define VLOG_TABLE_H_
 #include <vector>
 #include <tuple>
+#include <map>
 
-template<typename T>
-class Property {
-private:
-    std::string label_;
-    T value_;
-public:
+namespace otvdb
+{
 
-    
-
-
-};
-
-template<class... Properties>
+template<typename... Properties>
 class Table
 {
 private:
     /* data */
-    std::vector<std::tuple<Properties>> table_;
-    std::vector<std::string> labels_;
+    template<typename T> std::map<T, std::tuple<Properties...>> table_;
+    std::vector<const char*> labels_;
 public:
     Table() = delete;
-    template<class... Properties> Table();
+    template<typename... Properties> Table(std::vector<const char*> _label_list, const char *_key_label){
+        
+    }
     ~Table();
 
-    void insert(std::tuple<Properties> _entry);
+    void insert(std::tuple<Properties...> _entry);
 
 };
+
+template<typename... Properties>
+Table::Table(std::vector<const char*> _label_list, const char *_key_label){
+    if (sizeof ...(Properties) != sizeof(_label_list)){
+        /* code */
+    }
+    
+}   
+
+} // namespace otvdb
+
+
 
 #endif // !VLOG_TABLE_H_
