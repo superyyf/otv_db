05e99fed9cb5b088bc6cceecbb87d50a1e73625f
superyyf <1420328500@qq.com>
2023-11-20 08:07:48 +0800

    add print tuple

--- include/otvdb_table.h
+++ include/otvdb_table.h
@@ -5,6 +5,7 @@
 #include <set>
 #include <cassert>
 #include <iostream>
+#include <utility>
 
 namespace otvdb
 {
@@ -28,27 +29,41 @@ public:
 
 template<typename... Properties>
 Table<Properties...>::Table(std::initializer_list<const char*> _label_list){
+    std::cout << sizeof(_label_list) << "," << sizeof ...(Properties) << std::endl;
     assert((sizeof ...(Properties) != 0));
-    assert((sizeof(_label_list) != 0));
-    assert((sizeof(_label_list) == sizeof ...(Properties)));
+    assert((_label_list.size() != 0));
+    assert((_label_list.size() == sizeof ...(Properties)));
     labels_ = _label_list;
-}   
+}
+
+template<typename... Properties>
+Table<Properties...>::~Table(){} 
 
 template<typename... Properties>
 void Table<Properties...>::insert(std::tuple<Properties...> _entry){
     table_.insert(_entry);
 }
 
+template<std::size_t I = 0, typename... Tp>
+inline typename std::enable_if<I == sizeof...(Tp), void>::type
+print(const std::tuple<Tp...>& t)
+{ }
+
+template<std::size_t I = 0, typename... Tp>
+inline typename std::enable_if<I < sizeof...(Tp), void>::type
+print(const std::tuple<Tp...>& t)
+{
+    std::cout << std::get<I>(t) << std::endl;
+    print<I + 1, Tp...>(t);
+}
+
 template<typename... Properties>
 void Table<Properties...>::show(){
-    int n = std::tupe_size<std::tuple<Properties...>>;
     for(auto p = table_.begin(); p != table_.end(); ++p){
-        for(int i = 0; i < n; ++i){
-            std::cout << std::get<i>(*p) << '\t';
-        }
-        std::cout << std::endl;
+        print(*p);
     }
 }
+
 
 } // namespace otvdb
 
